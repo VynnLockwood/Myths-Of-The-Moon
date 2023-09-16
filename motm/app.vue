93ac81d803783fe0329da03aa00332c9363d665a
VynnLockwood <nolanrobinson490@gmail.com>
2023-09-17 00:45:41 +0700

    Install Nuxt 3 and Tailwind

---
+++ motm/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    Navmyths />
+  </div>
+</template>
---
+++ motm/components/Navmyths.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <ul>
+            <li><a href="#" class="font-bold underline"></a>Home</li>
+            <li><a href="#"></a>Services</li>
+        </ul>
+    </div>
+</template>
